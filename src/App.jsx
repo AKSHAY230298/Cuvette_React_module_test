d0090b50526d5f0cb545caa02ff7334427788e92
AKSHAY230298 <akshu.pokalekar98@gmail.com>
2024-10-09 22:25:43 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+// import Home from './Pages/Home';
+import React from 'react';
+import HomeP from './Components/Home';
+
+function App() {
+  return (
+    <>
+      <div style={{ display: 'flex' }}>
+      <HomeP />
+      </div>
+    </>
+  );
+}
+
+export default App;
